69c765e1f1f8aebb621aae27b1ff1b03a3944fc5
jrafaaael <jrmd070102@gmail.com>
2021-07-26 10:17:10 -0400

    added custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "stars-pattern": "url('./src/assets/background-stars.svg')",
+      }),
+      height: {
+        custom: "0.1rem",
+        lessNavbar: "calc(100% - 4rem)"
+      },
+      textColor: {
+        snow: "#fffafa",
+      },
+      gridTemplateColumns: {
+        responsive: "repeat(auto-fit, minmax(min(100%, 300px), 1fr));",
+      },
+    },
   },
   variants: {
     extend: {},
