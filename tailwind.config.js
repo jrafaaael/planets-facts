cc5c77cd0afcd9af0ee72bf9e43efc8884ff19c2
jrafaaael <jrmd070102@gmail.com>
2021-07-26 11:43:37 -0400

    remove unused styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        custom: "0.1rem",
+        lessNavbar: "calc(100% - 4rem)"
+      },
+      textColor: {
+        snow: "#fffafa",
+      },
+      gridTemplateColumns: {
+        responsive: "repeat(auto-fit, minmax(min(100%, 300px), 1fr));",
+      },
+    },
   },
   variants: {
     extend: {},
