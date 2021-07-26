d52515ff5b30131a3b26099979297f5ccabc31fe
jrafaaael <jrmd070102@gmail.com>
2021-07-26 11:42:35 -0400

    Revert "added custom styles"
    This reverts commit 69c765e1f1f8aebb621aae27b1ff1b03a3944fc5.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,7 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      backgroundImage: (theme) => ({
-        "stars-pattern": "url('./src/assets/background-stars.svg')",
-      }),
-      height: {
-        custom: "0.1rem",
-        lessNavbar: "calc(100% - 4rem)"
-      },
-      textColor: {
-        snow: "#fffafa",
-      },
-      gridTemplateColumns: {
-        responsive: "repeat(auto-fit, minmax(min(100%, 300px), 1fr));",
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
