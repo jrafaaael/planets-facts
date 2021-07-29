90421efb22ff77b31d76eb588f2556cd643f942c
jrafaaael <jrmd070102@gmail.com>
2021-07-28 21:45:14 -0400

    formating code. added styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./views/App/App";
+import "./styles/global.css";
+import "./styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
