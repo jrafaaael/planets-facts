c3c8a4a612e28253f4eb28d5bd61b12c73cc6128
jrafaaael <jrmd070102@gmail.com>
2021-07-30 23:16:17 -0400

    added new lottie animation for handle loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,14 +1,14 @@
-import Lottie from "react-lottie-player";
-import space_tour from "../../assets/lottie/space-tour.json";
+import Lottie from "lottie-react";
+import planet_line_loading from "../../assets/lottie/planet-line-loading.json";
 
 export const Loading = () => {
   return (
-    <section className="max-w-2xl m-auto h-full">
+    <section className="max-w-2xl m-auto h-full flex flex-col justify-center items-center">
       <Lottie
         loop
-        animationData={space_tour}
-        play
-        className="m-auto md:w-1/2"
+        autoplay
+        animationData={planet_line_loading}
+        className="w-1/2"
       />
       <h2 className="text-2xl text-center">Loading...</h2>
     </section>
