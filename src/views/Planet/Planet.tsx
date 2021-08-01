56008094379976549f7a00855a5933a4b165a35a
jrafaaael <jrmd070102@gmail.com>
2021-08-01 09:17:12 -0400

    added planet view

---
+++ src/views/Planet/Planet.tsx
@@ -0,0 +1,47 @@
+import { ChangeEvent, useState } from "react";
+import { Redirect } from "wouter";
+import { ImageChanger } from "../../components/ImageChanger/ImageChanger";
+import { Loading } from "../../components/Loading/Loading";
+import { PlanetFacts } from "../../components/PlanetFacts/PlanetFacts";
+import { PlanetImage } from "../../components/PlanetImage/PlanetImage";
+import { useGetPlanet } from "../../hooks/useGetPlanet";
+import { Option, OPTIONS } from "../../types/PlanetImageOptions";
+import { PLANET, PLANETS } from "../../types/Planets";
+
+interface Props {
+  params: {
+    planet: PLANET;
+  };
+}
+
+export const Planet = ({ params: { planet } }: Props) => {
+  const { loading, data, error } = useGetPlanet(planet);
+  const [selectedOption, setSelectedOption] = useState<Option>(OPTIONS[0]);
+
+  const handleChange = (e: ChangeEvent) => {
+    const option = OPTIONS.find(
+      (option) => option.value === (e.target as HTMLInputElement).value
+    )!;
+    setSelectedOption(option);
+  };
+
+  return (
+    <>
+      {!PLANETS.includes(planet) && <Redirect to="/" />}
+      {loading && <Loading />}
+      {data && (
+        <section className="max-w-4xl m-auto grid gap-4 md:grid-cols-2 md:grid-rows-[1fr,auto] md:gap-x-10 lg:h-full lg:grid-rows-[auto,auto]">
+          <ImageChanger
+            options={OPTIONS}
+            current={selectedOption.value}
+            color={selectedOption.color}
+            handleChange={handleChange}
+          />
+          <PlanetImage planet={planet} type={selectedOption.value} />
+          <PlanetFacts facts={data} />
+        </section>
+      )}
+      {error && <pre>{JSON.stringify(error, null, "\t")}</pre>}
+    </>
+  );
+};
